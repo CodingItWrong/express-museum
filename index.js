93447a458057a4fda981cd599d3621742d49a456
Josh Justice <me@codingitwrong.com>
2019-08-10 07:04:22 -0400

    Add mongo

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const rest = require('./routes/rest')
 const memory = require('./repos/memory')
 const sql = require('./repos/sql')
+const mongo = require('./repos/mongo')
 
 let app = express()
 
 app.use('/rest-memory', rest(memory))
 app.use('/rest-sql', rest(sql))
+app.use('/rest-mongo', rest(mongo))
 
 app.listen(3000)
---
+++ repos/mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/express-museum', {
+  useNewUrlParser: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+
+const restaurantSchema = new mongoose.Schema({
+  name: String,
+})
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema)
+
+Restaurant.find()
+  .exec()
+  .then(response => {
+    if (response.length === 0) {
+      const sushiPlace = new Restaurant({ name: 'Sushi Place' })
+      const burgerPlace = new Restaurant({ name: 'Burger Place' })
+
+      sushiPlace.save(err => err && console.error(err))
+      burgerPlace.save(err => err && console.error(err))
+    }
+  })
+
+const repo = {
+  all: () => Restaurant.find(),
+}
+
+module.exports = repo
