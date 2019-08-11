79188ba2cbe4e99b7061344e887f93c8d6d6ffff
Josh Justice <me@codingitwrong.com>
2019-08-10 20:01:56 -0400

    Separate normal and serverless config

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const rest = require('./routes/rest')
+const jsonapi = require('./routes/jsonapi')
+const graphql = require('./routes/graphql')
+
+const memory = require('./repos/memory')
+// const sql = require('./repos/sql')
+const mongo = require('./repos/mongo')
+
+let app = express()
+
+app.use('/rest-memory', rest(memory))
+// app.use('/rest-sql', rest(sql))
+app.use('/rest-mongo', rest(mongo))
+
+app.use('/jsonapi-memory', jsonapi(memory))
+// app.use('/jsonapi-sql', jsonapi(sql))
+app.use('/jsonapi-mongo', jsonapi(mongo))
+
+graphql(memory).applyMiddleware({ app, path: '/graphql-memory' })
+// graphql(sql).applyMiddleware({ app, path: '/graphql-sql' })
+graphql(mongo).applyMiddleware({ app, path: '/graphql-mongo' })
+
+module.exports = app
--- index.js
+++ index.js
@@ -2,29 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-const express = require('express')
-const serverless = require('serverless-http')
+const app = require('./app')
 
-const rest = require('./routes/rest')
-const jsonapi = require('./routes/jsonapi')
-const graphql = require('./routes/graphql')
-
-const memory = require('./repos/memory')
-const sql = require('./repos/sql')
-const mongo = require('./repos/mongo')
-
-let app = express()
-
-app.use('/rest-memory', rest(memory))
-app.use('/rest-sql', rest(sql))
-app.use('/rest-mongo', rest(mongo))
-
-app.use('/jsonapi-memory', jsonapi(memory))
-app.use('/jsonapi-sql', jsonapi(sql))
-app.use('/jsonapi-mongo', jsonapi(mongo))
-
-graphql(memory).applyMiddleware({ app, path: '/graphql-memory' })
-graphql(sql).applyMiddleware({ app, path: '/graphql-sql' })
-graphql(mongo).applyMiddleware({ app, path: '/graphql-mongo' })
-
-module.exports.handler = serverless(app)
+const port = process.env.PORT || 80
+app.listen(port)
